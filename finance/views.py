7f68420277117a5ae3e969aaba614e8fb6dc8a22
AnuragException <raianurag231@gmail.com>
2022-10-30 00:24:23 +0530

    Added html pages in finance section

--- finance/views.py
+++ finance/views.py
@@ -5,17 +5,26 @@ from django.contrib.auth.decorators import login_required
 from finance.decorators import finance_required,finance_manager_required
 
 class Index(View):
-
     @method_decorator(login_required)
     @method_decorator(finance_required)
     def get(self,request):
-        return render(request,'finance/index.html',{})
-
-
-
+        return render(request,'finance/base.html',{})
 
 class FinanceProfile(View):
     @method_decorator(login_required)
     @method_decorator(finance_required)
     def get(self,request):
         return render(request,'finance/profile.html',{})
+
+class AccountProfile(View):
+    @method_decorator(login_required)
+    @method_decorator(finance_required)
+    @method_decorator(finance_manager_required)
+    def get(self,request):
+        return render(request,'finance/Account.html',{})        
+
+class StudentProfile(View):
+    @method_decorator(login_required)
+    @method_decorator(finance_required)
+    def get(self,request):
+        return render(request,'finance/studentprofile.html',{}) 
