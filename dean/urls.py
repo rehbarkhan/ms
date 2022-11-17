c7eab9048a6b4d6348f8153d187eba4b208ce5ea
rehbarkhan <md.rehbar@outlook.com>
2022-11-17 12:08:50 +0530

    added the functionality of updating the existing course

--- dean/urls.py
+++ dean/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from dean.views import (
     Index,AdmissionApproval,AdmissinApprovalLink,
-    CourseDetails,FinanceApprove,FinanceApproval
+    CourseDetails,FinanceApprove,FinanceApproval,
+    EditCourse
 )
 app_name = 'dean'
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('admissionapr/<str:u_name>/',AdmissinApprovalLink.as_view(),name='admission_apr_link'),
     path('courses/',CourseDetails.as_view(),name='course'),
     path('finance/',FinanceApprove.as_view(),name='financestaff'),
-    path('financeapr/<int:pk>/',FinanceApproval.as_view(),name='financeapproval')
+    path('financeapr/<int:pk>/',FinanceApproval.as_view(),name='financeapproval'),
+    path('editcourse/<int:pk>/',EditCourse.as_view(),name='edit-course'),
 ]
